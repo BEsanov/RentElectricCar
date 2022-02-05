1702293312e5197f1bf4205aead231e4cf39a432
Bakhtiyor Esanov <bahtiyoresanov@gmail.com>
2022-02-05 08:55:32 +0200

    CarDto.cs Outer Facing Model and CarsProfile added

---
+++ RentElectricCar.API/Models/CarDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RentElectricCar.API.Models
+{
+    public class CarDto
+    {
+        public Guid CarId { get; set; }
+        public string ColorName { get; set; }
+        public int MileAge { get; set; }
+
+        public string Transmission { get; set; }
+        public DateTimeOffset ProductionDate { get; set; }
+        public int NumberOfDoors { get; set; }
+        public int NumberOfSeats { get; set; }
+
+        public Guid LocationId { get; set; }
+    }
+}
---
+++ RentElectricCar.API/Profiles/CarsProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace RentElectricCar.API.Profiles
+{
+    public class CarsProfile:Profile
+    {
+        public CarsProfile()
+        {
+            CreateMap<Entities.Car, Models.CarDto>();
+        }
+    }
+}
