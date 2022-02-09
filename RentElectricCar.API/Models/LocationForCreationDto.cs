f0e0917c6f73dda03539f1c04b10682a9d8af2a5
Bakhtiyor Esanov <bahtiyoresanov@gmail.com>
2022-02-09 21:30:14 +0200

    Creating Location with car(s) together- Creating child resources together with parent the parent resource

--- RentElectricCar.API/Models/LocationForCreationDto.cs
+++ RentElectricCar.API/Models/LocationForCreationDto.cs
@@ -1,8 +1,13 @@
-﻿namespace RentElectricCar.API.Models
+﻿using System.Collections.Generic;
+
+namespace RentElectricCar.API.Models
 {
     public class LocationForCreationDto
     {
         public string RegionName { get; set; }
         public string CityName { get; set; }
+
+        public ICollection<CarsForCreationDto> Cars { get; set; }
+            = new List<CarsForCreationDto>();
     }
 }
