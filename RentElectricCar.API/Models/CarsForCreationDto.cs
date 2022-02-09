a04ecfbb47eca60e2c7d902e0b699b701e626ef9
Bakhtiyor Esanov <bahtiyoresanov@gmail.com>
2022-02-09 21:59:42 +0200

    Validating Input with DataAnnotations- Implementing IValidatableObject contract(interface).

--- RentElectricCar.API/Models/CarsForCreationDto.cs
+++ RentElectricCar.API/Models/CarsForCreationDto.cs
@@ -1,15 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentElectricCar.API.Models
 {
-    public class CarsForCreationDto
+    public class CarsForCreationDto:IValidatableObject
     {
+        [Required]
+        [MaxLength(150)]
         public string CarBrandName { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string ColorName { get; set; }
+        [Required]
+        [Range(0, 500000)]
         public int MileAge { get; set; }
+        [Required]
         public DateTimeOffset ProductionDate { get; set; }
+        [Required]
+        [Range(0, 10)]
         public int NumberOfDoors { get; set; }
+        [Required]
+        [Range(0, 10)]
         public int NumberOfSeats { get; set; }
-      
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+
+            //need improvement here 
+            if (CarBrandName==null)
+            {
+                yield return new ValidationResult("Provide Car Brand name.",
+                    new[] { "CarsForCreationDto" }); 
+            }   
+        }
     }
 }
